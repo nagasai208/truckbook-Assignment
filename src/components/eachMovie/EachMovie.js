fd8e8bb88a7b602692fb7797013986b114f5f5fc
sai1 <koppuravurinagasai@gmail.com>
2021-05-21 15:31:04 +0530

    second-commit

---
+++ src/components/eachMovie/EachMovie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EachMovie extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default EachMovie;
---
+++ src/components/moviesDashboard/MoviesDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoviesDashboard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MoviesDashboard;
